f350e3de63918a1a78564da881057eab1d34320f
Esteban Manchado Velázquez <esteban@redhat.com>
2020-03-16 10:51:40 +0100

    Use release ZIP files instead of dist/
    Remove the dist/ directory from the repo, and add a Grunt target to
    create a ZIP file for the current release (taking the version number
    from plugin.json).

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var child_process = require('child_process'),
+    fs = require('fs');
+
 module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
@@ -7,7 +10,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    clean: ["dist"],
+    clean: ["dist", "grafana-plugin-link-singlestat"],
 
     copy: {
       src_to_dist: {
@@ -20,6 +23,12 @@ module.exports = function(grunt) {
         expand: true,
         src: [ 'plugin.json', 'README.md' ],
         dest: 'dist',
+      },
+      zip: {
+        cwd: 'dist',
+        expand: true,
+        src: ['**/*'],
+        dest: 'grafana-plugin-link-singlestat',
       }
     },
 
@@ -46,9 +55,16 @@ module.exports = function(grunt) {
           ext:'.js'
         }]
       },
-    },
+    }
 
   });
 
   grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:pluginDef', 'babel']);
+  grunt.registerTask('zip', ['default', 'copy:zip', 'zip-create', 'clean']);
+  grunt.registerTask('zip-create', 'Create a release ZIP file (needs version number)', function() {
+    var jsonData = fs.readFileSync("plugin.json");
+    var version = JSON.parse(jsonData).info.version;
+    grunt.log.writeln('zip -9r "grafana-plugin-link-singlestat-' + version + '.zip" grafana-plugin-link-singlestat/');
+    child_process.execSync('zip -9r "grafana-plugin-link-singlestat-' + version + '.zip" grafana-plugin-link-singlestat/');
+  });
 };
--- README.md
+++ README.md
@@ -8,3 +8,11 @@ differences:
 
 ![Example usage (the plugin below is singlestat)](screenshots/link-singlestat-usage.png)
 ![Configuration panel](screenshots/link-singlestat-config.png)
+
+
+## Making a release
+
+If you want a ZIP file for the current release, type `grunt zip`. That
+will leave a file called `grafana-plugin-link-singlestat-VERSION.zip`
+in the current directory, where `VERSION` is the version number in
+`plugin.json`.
